115eae78e1deb83953b6cebaee33a2d3ac77c38a
miyazi777 <m71203@gmail.com>
2019-05-22 23:22:55 +0900

    first commit

---
+++ git/git.go
@@ -0,0 +1,33 @@
+package git
+
+import (
+	"bytes"
+	"errors"
+	"os/exec"
+	"strings"
+)
+
+// カレントブランチ取得
+func GetCurrentBranch() (string, error) {
+	cmd := exec.Command("git", "symbolic-ref", "--short", "HEAD")
+	var stdout bytes.Buffer
+	cmd.Stdout = &stdout
+	err := cmd.Run()
+	if err != nil {
+		return "", errors.New("Not a git repository")
+	}
+
+	branchName := stdout.String()
+	return strings.Trim(branchName, "\n"), nil
+}
+
+// 説明追加
+func SetDescription(branchName string, desc string) error {
+	key := "branch." + branchName + ".description"
+	cmd := exec.Command("git", "config", "--local", key, desc)
+	err := cmd.Run()
+	if err != nil {
+		return errors.New("Not a git repository")
+	}
+	return nil
+}
