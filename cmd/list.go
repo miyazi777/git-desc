029a5d8a108878fcb03e6ed0e799f01bb198f4ff
miyazi777 <m71203@gmail.com>
2019-05-26 18:07:02 +0900

    modify list command

--- cmd/list.go
+++ cmd/list.go
@@ -18,26 +18,28 @@ import (
 	"fmt"
 
 	"gitdesc/git"
+	"github.com/InVisionApp/tabular"
 	"github.com/spf13/cobra"
 )
 
 // listCmd represents the list command
 var listCmd = &cobra.Command{
 	Use:   "list",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Information descriptions",
+	Long:  `Information branch descriptions.`,
 	RunE: func(cmd *cobra.Command, args []string) error {
 		descriptionMap, err := git.BuildDescriptionMap()
 		if err != nil {
 			return err
 		}
+
+		tab := tabular.New()
+		tab.Col("branch", "Branch", 30)
+		tab.Col("desc", "Description", 40)
+		format := tab.Print("*")
+
 		for branchName, description := range descriptionMap {
-			fmt.Printf("%s : %s\n", branchName, description)
+			fmt.Printf(format, branchName, description)
 		}
 		return nil
 	},
