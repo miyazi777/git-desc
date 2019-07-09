3e4301f8eb73cc7df7cceda1c589d1c11b285d2b
miyazi777 <m71203@gmail.com>
2019-07-09 23:15:05 +0900

    change format for branch list

--- cmd/list.go
+++ cmd/list.go
@@ -1,14 +1,8 @@
 package cmd
 
 import (
-	"errors"
-	//"fmt"
 	"github.com/cheynewallace/tabby"
-	//"github.com/InVisionApp/tabular"
-	"github.com/mattn/go-runewidth"
-	"github.com/miyazi777/git-desc/shell"
 	"github.com/spf13/cobra"
-	"github.com/spf13/viper"
 )
 
 // listCmd represents the list command
@@ -30,21 +24,6 @@ func execute(cmd *cobra.Command, args []string) error {
 		return err
 	}
 
-	width, err := shell.GetWidth()
-
-	branchWidthPer, err := getPercentConfig("list.branch.width")
-	if err != nil {
-		return err
-	}
-
-	descWidthPer, err := getPercentConfig("list.description.width")
-	if err != nil {
-		return err
-	}
-
-	branchWidth := int(float64(width) * branchWidthPer)
-	descWidth := int(float64(width) * descWidthPer)
-
 	onlyListFlg, _ := cmd.PersistentFlags().GetBool("only-list")
 	t := tabby.New()
 	if !onlyListFlg {
@@ -52,21 +31,8 @@ func execute(cmd *cobra.Command, args []string) error {
 	}
 
 	for branchName, description := range descriptionMap {
-		t.AddLine(runewidth.Truncate(branchName, branchWidth, "..."), runewidth.Truncate(description, descWidth, "..."))
+		t.AddLine(branchName, description)
 	}
 	t.Print()
 	return nil
-}
-
-func getPercentConfig(key string) (float64, error) {
-	checkVal := viper.Get(key)
-	if checkVal == nil {
-		return 0, errors.New("Require error: " + key)
-	}
-
-	val := viper.GetFloat64(key)
-	if val < 0.0 || val > 1.00 {
-		return 0, errors.New("Range error: " + key)
-	}
-	return val, nil
 }
